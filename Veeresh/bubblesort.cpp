48c5c750819137c1f657f013035553030101163e
VeereshMattikalli <114333895+VeereshMattikalli@users.noreply.github.com>
2024-12-28 18:47:46 +0530

    Update bubblesort.cpp

--- Veeresh/bubblesort.cpp
+++ Veeresh/bubblesort.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int A[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (A[j + 1] < A[j]) {
+                int temp = A[j];
+                A[j] = A[j + 1];
+                A[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int A[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(A) / sizeof(A[0]);
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(A, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
 
